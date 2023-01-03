66855261e3bc955b9c00aeb80debdc36bf16d79e
ArtemE <qgren@ukr.net>
2023-01-03 11:50:20 +0200

    Adding a security system

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,2 +1,16 @@
+def is_math(a: str) -> bool:
+    list_num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", "*"]
+    a = list(a)
+    for elem in a:
+        if elem not in list_num:
+            return False
+    return True
+
+
 while True:
-    print("результат: "+str(eval(input("высчитать: "))))
+    task = input("Высчитать: ")
+    if is_math(task):
+        print("Результат: "+str(eval(task)))
+
+
+
